8c9bd9e5535c8bc585fa50b6f31054485bb43a90
Brian McAllister <brian.mcallister@lab49.com>
2022-08-16 20:16:17 -0400

    Better validation logic using zod.

--- pages/api/cursor.ts
+++ pages/api/cursor.ts
@@ -1,70 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sql } from "slonik";
+import { z } from "zod";
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
 
-import { FilterableFields, isFilterableField } from "../../domain/FilterableFields";
+import {
+  FilterableFields,
+  isFilterableField,
+} from "../../domain/FilterableFields";
 import { Transaction } from "../../domain/Transaction";
 import { createLimitFragment } from "../../utils/createLimitFragment";
 import { createOrderByFragment } from "../../utils/createOrderByFragment";
 import { createPool } from "../../utils/createPool";
 import { createWhereFragment } from "../../utils/createWhereFragment";
 
+const Pagination = z.strictObject({
+  cursor: z.string().optional().default("1"),
+  offset: z.string().optional().default("0"),
+  limit: z.string().optional().default("10"),
+  order: z.enum(["asc", "desc"]).optional(),
+});
+
+const Filterable = z.strictObject({
+  code: z.string().optional(),
+  type: z.string().optional(),
+  amount: z.string().optional(),
+  description: z.string().optional(),
+  date: z.string().optional(),
+});
+
+const Query = Pagination.merge(Filterable);
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {
-    cursor = "1",
-    offset = "0",
-    limit = "10",
-    amount,
-    description,
-    order = "asc",
-    code = "",
-  } = req.query;
+  try {
+    const query = Query.parse(req.query);
+    const { cursor, order, limit, offset } = query;
+    const pool = await createPool();
+    const toFilter = Object.entries(query).filter<[FilterableFields, string]>(
+      (val): val is [FilterableFields, string] => isFilterableField(val[0])
+    );
 
-  const pool = await createPool();
+    pool.connect(async (conn) => {
+      const query = sql<Transaction>`
+        SELECT * FROM transactions
+        ${createWhereFragment(parseInt(cursor, 10), toFilter)}
+        ${createOrderByFragment("id", order)}
+        ${createLimitFragment(parseInt(limit, 10), parseInt(offset, 10))}
+      `;
+      const data = await conn.many(query);
 
-  if (
-    Array.isArray(req.query.code) ||
-    Array.isArray(req.query.type) ||
-    Array.isArray(req.query.amount) ||
-    Array.isArray(req.query.description) ||
-    Array.isArray(req.query.date) ||
-    Array.isArray(order) ||
-    Array.isArray(cursor) ||
-    Array.isArray(offset) ||
-    Array.isArray(limit) ||
-    Array.isArray(amount) ||
-    Array.isArray(description)
-  ) {
-    throw new Error("bad request");
-  }
-
-  if (order !== "asc" && order !== "desc") {
-    throw new Error("bad request");
-  }
-
-  const toFilter = Object.keys(req.query)
-    .filter<FilterableFields>(isFilterableField)
-    .map<[FilterableFields, string]>((field) => {
-      const f = req.query[field];
-
-      if (!f) {
-        throw new Error("bad request");
-      }
-
-      return [field, Array.isArray(f) ? f.join() : f];
+      res.status(StatusCodes.OK).json({ data });
     });
-
-  pool.connect(async (conn) => {
-    const query = sql<Transaction>`
-      SELECT * FROM transactions
-      ${createWhereFragment(parseInt(cursor, 10), toFilter)}
-      ${createOrderByFragment("id", order)}
-      ${createLimitFragment(parseInt(limit, 10), parseInt(offset, 10))}
-    `;
-    const data = await conn.many(query);
-
-    res.status(200).json({ data });
-  });
+  } catch (err) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(getReasonPhrase(StatusCodes.BAD_REQUEST));
+  }
 }
