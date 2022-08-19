6419bcac39c899b70a070e27f6e6019627764cd0
Brian McAllister <brian.mcallister@lab49.com>
2022-08-19 17:21:33 -0400

    Handle reverse cursor pagination.

--- pages/components/Grid.tsx
+++ pages/components/Grid.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { AgGridReact } from "@ag-grid-community/react";
 import { ModuleRegistry, IServerSideDatasource } from "@ag-grid-community/core";
 import { ServerSideRowModelModule } from "@ag-grid-enterprise/server-side-row-model";
@@ -10,75 +10,109 @@ import "@ag-grid-community/styles/ag-theme-alpine.css";
 
 ModuleRegistry.register(ServerSideRowModelModule);
 
-const PER_PAGE_COUNT = 50;
+const PER_PAGE_COUNT = 15;
 
-const offsetDatasource: IServerSideDatasource = {
-  getRows: ({ request, success, fail }) => {
-    const url = new URL("/api/offset", window.location.origin);
-    const { startRow, sortModel } = request;
-    const params = new URLSearchParams();
+const fetchTransactions = async (url: URL): Promise<Transaction[]> => {
+  const resp = await fetch(url);
+  const { data, errors } = await resp.json();
+  const txn = data as Transaction[];
+  const sorted = txn.sort((a, b) => a.id - b.id);
 
-    params.set("limit", `${PER_PAGE_COUNT}`);
-    params.set("offset", `${startRow}`);
-
-    if (sortModel.length) {
-      const { sort, colId } = sortModel[0];
-
-      params.set("sortBy", colId);
-      params.set("sortDir", sort);
-    }
-
-    url.search = params.toString();
-
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((resp) => {
-        success({ rowData: resp.data });
-      })
-      .catch((err) => {
-        console.log("error fetching data", err);
-        fail();
-      });
-  },
+  if (resp.ok) {
+    return Promise.resolve(sorted);
+  } else {
+    return Promise.reject(`some error: ${errors}`);
+  }
 };
 
-const cursorDatasource: IServerSideDatasource = {
-  getRows: ({ request, api, success, fail }) => {
-    const url = new URL("/api/cursor", window.location.origin);
-    const { sortModel } = request;
-    const params = new URLSearchParams();
-    // TODO (brianmcallister) - This is incorrect. AG Grid gets confused because
-    // when we run this line of code, we don't know if we're scrolling up or down.
-    // Which means we don't know what the _next_ page of data to actually
-    // get would be (i.e. if we're scrolling up we want to get the previous page
-    // and if we're scrolling down we want the next page).
-    const cursor = api.getDisplayedRowAtIndex(api.getLastDisplayedRow() - 1)
-      ?.data?.id;
+const createOffsetDatasource = () => {
+  const offsetDatasource: IServerSideDatasource = {
+    getRows: ({ request, success, fail }) => {
+      const url = new URL("/api/offset", window.location.origin);
+      const { startRow, sortModel } = request;
+      const params = new URLSearchParams();
 
-    if (cursor) {
-      params.set("cursor", cursor);
-    }
+      params.set("limit", `${PER_PAGE_COUNT}`);
+      params.set("offset", `${startRow}`);
 
-    params.set("limit", `${PER_PAGE_COUNT}`);
+      if (sortModel.length) {
+        const { sort, colId } = sortModel[0];
 
-    if (sortModel.length) {
-      const { sort } = sortModel[0];
+        params.set("sortBy", colId);
+        params.set("sortDir", sort);
+      }
 
-      params.set("sortDir", sort);
-    }
+      url.search = params.toString();
 
-    url.search = params.toString();
+      fetchTransactions(url)
+        .then((rowData) => {
+          success({ rowData });
+        })
+        .catch((err) => {
+          console.log("error fetching data", err);
+          fail();
+        });
+    },
+  };
 
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((resp) => {
-        success({ rowData: resp.data });
-      })
-      .catch((err) => {
-        console.log("error fetching data", err);
-        fail();
-      });
-  },
+  return offsetDatasource;
+};
+
+const createCursorDatasource = () => {
+  let previousHighestCacheBlock: number = 0;
+
+  const datasource: IServerSideDatasource = {
+    getRows: ({ request, api, success, fail }) => {
+      const url = new URL("/api/cursor", window.location.origin);
+      const { sortModel } = request;
+      const params = new URLSearchParams();
+
+      params.set("limit", `${PER_PAGE_COUNT}`);
+
+      // Calculate if the user is scrolling up or down. We need to know this
+      // information in order to request the next or previous page
+      // of data.
+      const sorted = Object.keys(api.getCacheBlockState()).sort(
+        (a, b) => parseInt(a, 10) - parseInt(b, 10)
+      );
+      const highestCacheBlock = parseInt(sorted[sorted.length - 1], 10);
+      const isScrollingDown = previousHighestCacheBlock < highestCacheBlock;
+
+      // Cache for the next getRows call.
+      previousHighestCacheBlock = highestCacheBlock;
+
+      // Find the id of the extant row to use as a cursor, depending on direction.
+      const nextIndex = isScrollingDown
+        ? api.getLastRenderedRow() - 1
+        : api.getFirstRenderedRow() + 1;
+      const cursor = api.getDisplayedRowAtIndex(nextIndex)?.data?.id;
+
+      if (cursor) {
+        params.set("cursor", cursor);
+        params.set("offset", "1");
+        params.set("sortDir", isScrollingDown ? "asc" : "desc");
+      }
+
+      if (sortModel.length) {
+        const { sort } = sortModel[0];
+
+        params.set("sortDir", sort);
+      }
+
+      url.search = params.toString();
+
+      fetchTransactions(url)
+        .then((rowData) => {
+          success({ rowData });
+        })
+        .catch((err) => {
+          console.log("error fetching data", err);
+          fail();
+        });
+    },
+  };
+
+  return datasource;
 };
 
 interface Props {
@@ -95,7 +129,8 @@ const columnDefs = [
 
 export const Grid = ({ type }: Props) => {
   const hasSortableColumns = type !== "cursor";
-  const datasource = type === "cursor" ? cursorDatasource : offsetDatasource;
+  const datasource =
+    type === "cursor" ? createCursorDatasource() : createOffsetDatasource();
   const defaultColDef = useMemo(
     () => ({
       sortable: hasSortableColumns,
@@ -107,7 +142,7 @@ export const Grid = ({ type }: Props) => {
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
       <AgGridReact<Transaction>
-        debug={true}
+        debug={false}
         getRowId={(row) => `${row.data.id}`}
         columnDefs={columnDefs}
         defaultColDef={defaultColDef}
